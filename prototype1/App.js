7933de40f0dc235c757e96c30b65d30c2515f5bf
Henno0701 <henno.passchier@gmail.com>
2024-02-19 18:15:45 +0100

    Layout of navigation created for mobile

--- prototype1/App.js
+++ prototype1/App.js
@@ -1,20 +1,9 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './screens/Home';
-import ProfileScreen from './screens/Profile';
-
-const Stack = createNativeStackNavigator();
+import Layout from './components/Layout';
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+      <Layout />
   );
 }
 
---
+++ prototype1/screens/Stations.js
@@ -0,0 +1,9 @@
+import { Button, Text, View } from 'react-native';
+
+export default function StationsScreen() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Stations Screen</Text>
+      </View>
+    );
+  }
