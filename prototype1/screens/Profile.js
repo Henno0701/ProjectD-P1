022023e00b01b608d76daf06d102cf233e356494
Henno0701 <henno.passchier@gmail.com>
2024-02-20 12:43:41 +0100

    Made all the pages except home page black background

--- prototype1/screens/Profile.js
+++ prototype1/screens/Profile.js
@@ -2,8 +2,8 @@ import { Button, Text, View } from 'react-native';
 
 export default function ProfileScreen() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Profile Screen</Text>
+      <View className="flex-1 justify-center bg-main_bg_color items-center">
+        <Text className="text-xl text-[#fff]">Profile Screen</Text>
       </View>
     );
   }
--- prototype1/screens/Reservations.js
+++ prototype1/screens/Reservations.js
@@ -2,8 +2,8 @@ import { Button, Text, View } from 'react-native';
 
 export default function ReservationsScreen() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Reservations Screen</Text>
+      <View className="flex-1 justify-center bg-main_bg_color items-center">
+        <Text className="text-xl text-[#fff]">Reservations Screen</Text>
       </View>
     );
   }
