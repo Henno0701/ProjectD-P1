293864fba8344bb9967b866c7ea7fb83b3755b5f
MelvinHR <1025457@hr.nl>
2024-02-22 23:41:24 +0100

    Home page but with reservation info

--- prototype1/screens/Home.js
+++ prototype1/screens/Home.js
@@ -5,27 +5,76 @@ import { Button, View, Text, TouchableOpacity } from 'react-native';
 
 export default function HomeScreen({ navigation: { navigate } }) {
     var Name = "John";
+    var reservation = false;
+    var Battery = 10;
+    var License = "XN-0A2-B2";
+    var Station = "LP-01A";
+    var Charge = 10;
+
     return (
       <View className="flex-1 bg-main_bg_color">
         <Text className="text-schuberg_blue text-4xl ml-5 mt-28">Welcome</Text>
 
-        {/* needs function that checks the name of the user */}
+        {/* !needs function that checks the name of the user. */}
         <Text className="text-[#ffffff] text-xl ml-5">{Name}</Text>
 
-        {/* Here needs to be an if statement that shows info of reservation if there is one */}
+        {/* If there is a reservation show information about it. */}
+        {/* !needs function that checks if there is a reservation. */}
+        {!reservation?
+        <View>
+
+          {/* Here needs to be an if statement that shows info of reservation if there is one */}
+          {/* !needs function that checks Battery */}
+          <Text className="ml-5 mt-3 text-[#686868] text-sm">Car Status</Text>
+          <View className="mr-5 ml-5 w-auto h-20 flex flex-row">
+            <View className="h-auto mr-5 flex-1 bg-secondary_bg_color rounded-xl justify-center">
+              <Text className="ml-5 mb-1 text-[#ffffff] text-base">Battery</Text>
+              <Text className="ml-5 text-[#686868]">Power: {Battery}%</Text>
+            </View>
+
+            {/* Here needs to be an if statement that shows info of reservation if there is one */}
+            {/* !needs function that checks time left */}
+            <View className=" h-20 flex-1 bg-secondary_bg_color justify-center rounded-xl">
+              <Text className="text-[#ffffff] ml-5 text-base">Time left</Text>
+            </View>
+          </View>
+
+          {/* Here needs to be an if statement that shows info of reservation if there is one */}
+          {/* !needs function that checks information about the charging station */}
+          <View className="bg-secondary_bg_color w-auto h-auto ml-5 mr-5 justify-center mt-3 rounded-xl">
+            <Text className="ml-5 mb-3 mt-5 text-[#7C7C7C]">Info</Text>
+
+            <View className="flex flex-row">
+              <Text className="ml-5 text-[#686868]">Charging Station:</Text>
+              <Text className="ml-5 text-[#ffffff]">{Station}</Text>
+            </View>
+
+            <View className="flex flex-row">
+              <Text className="ml-5 text-[#686868]">Charging KW:</Text>
+              <Text className="ml-5 text-[#ffffff]">{Charge} kW</Text>
+            </View>
+
+            <View className="flex flex-row">
+              <Text className="ml-5 text-[#686868]">License Plate:</Text>
+              <Text className="ml-5 mb-5 text-[#ffffff]">{License}</Text>
+            </View>
+          </View>
+        </View>
+        :null}
+
 
 
         {/* Button that leads to Reservations */}
-        <Text className="text-[#ffffff6b] text-sm ml-5 mt-8 ">Quick Access</Text>
+        <Text className="text-[#686868] text-sm ml-5 mt-3">Quick Access</Text>
         <TouchableOpacity className="bg-secondary_bg_color w-auto h-20 ml-5 mr-5 justify-center mt-3 rounded-xl"
             onPress={() => navigate('Reservations')}>
-            <Text className="text-[#ffffff6b] ml-10">Make Reservation</Text>
+            <Text className="text-[#686868] ml-10">Make Reservation</Text>
         </TouchableOpacity>
 
         {/* example button */}
         <TouchableOpacity className="bg-secondary_bg_color w-auto h-20 ml-5 mr-5 justify-center mt-3 rounded-xl"
             onPress={() => navigate('Home')}>
-            <Text className="text-[#ffffff6b] ml-10">Example</Text>
+            <Text className="text-[#686868] ml-10">Example</Text>
         </TouchableOpacity>
 
       </View>
