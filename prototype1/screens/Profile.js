56e2c4a4d7cd403f54a83aa7f58b883a7c53099d
maruf128 <maruf@live.nl>
2024-03-04 15:22:20 +0100

    Gehele profile mooier gemaakt, functies bestaan alleen invullen code netter en nakijken wat te doen voor security en contact details

--- prototype1/screens/Profile.js
+++ prototype1/screens/Profile.js
@@ -4,13 +4,12 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 export default function ProfileScreen() {
   var secondary_bg_color = "#232323";
 
-  // Account Modal
+  // !--- BEGIN ACOUNT MODAL ---!
   const [isAccountModalVisible, setAccountModalVisible] = useState(false);
   const [editedAccountName, setEditedAccountName] = useState("");
   const [editedAccountImageURL, setEditedAccountImageURL] = useState("");
   const [accountName, setAccountName] = useState("Maruf Rodjan");
   const [accountImageURL, setAccountImageURL] = useState("https://imgur.com/4OxZys5.jpg");
-
   const handleAccountPress = () => setAccountModalVisible(true);
   const closeAccountModal = () => setAccountModalVisible(false);
   const handleAccountSave = () => {
@@ -30,31 +29,45 @@ export default function ProfileScreen() {
     // Close the modal
     closeAccountModal();
   };
-  // Contact Details Modal
+  // !--- END ACOUNT MODAL ---! 
+
+  // !--- BEGIN CONTACT DETAILS MODAL ---!
   const [isContactDetailsModalVisible, setContactDetailsModalVisible] = useState(false);
   const handleContactDetailsPress = () => setContactDetailsModalVisible(true);
   const closeContactDetailsModal = () => setContactDetailsModalVisible(false);
   const handleContactDetailsSave = () => {
-    // Logic for saving Contact details
-    // ...
-
+    // hier komt dan de logica indien er iets voor de contact details pagina komt
     // Close the modal
     closeContactDetailsModal();
   };
+  // !--- END CONTACT DETAILS MODAL ---!
+
+  // !--- BEGIN SECURITY MODAL ---!
+  const [isSecurityModalVisible, setSecurityModalVisible] = useState(false);
+  const handleSecurityPress = () => setSecurityModalVisible(true);
+  const closeSecurityModal = () => setSecurityModalVisible(false);
+  const handleSecuritySave = () => {
+    // hier komt dan de logica indien er iets voor de contact details pagina komt
+    // Close the modal
+    closeSecurityModal();
+  };
+  // !--- END SECURITY MODAL ---!
+
+  // USE EFFECT
   useEffect(() => {
     // laad de data via asycnstorage
     loadAccountData();
   }, []);
+  // USE EFFECT
 
+  // !--- ASYNC METHODS ---!
   const saveAccountData = async () => {
     try {
-      // Check if editedAccountName is not empty, if empty, use current accountName
+      // Check hier of een van de 2 data leeg is, zo ja gebruik de huidige gegeven
       const nameToSave = editedAccountName.trim() !== '' ? editedAccountName : accountName;
-
-      // Check if editedAccountImageURL is not empty, if empty, use current accountImageURL
       const imageURLToSave = editedAccountImageURL.trim() !== '' ? editedAccountImageURL : accountImageURL;
 
-      // Save the account data via AsyncStorage (later it will go through a database)
+      // Sla het dan op, later gaat het via JSON file en indien dit de taal word via een DB
       await AsyncStorage.setItem('accountName', nameToSave);
       await AsyncStorage.setItem('accountImageURL', imageURLToSave);
     } catch (error) {
@@ -64,23 +77,22 @@ export default function ProfileScreen() {
 
   const loadAccountData = async () => {
     try {
-      // Load account data via AsyncStorage
+      // Laat de data via asyncstorage
       const savedAccountName = await AsyncStorage.getItem('accountName');
       const savedAccountImageURL = await AsyncStorage.getItem('accountImageURL');
 
-      // Update the state with the loaded data if not null
+      // indien het niet null is laat het dan zien
       if (savedAccountName !== null) {
-        setAccountName(savedAccountName);
+        setEditedAccountName(savedAccountName);
       }
-
       if (savedAccountImageURL !== null) {
-        setAccountImageURL(savedAccountImageURL);
+        setEditedAccountImageURL(savedAccountImageURL);
       }
     } catch (error) {
       console.error('Error loading account data:', error);
     }
   };
-
+  // !--- END ASYNC METHODS ---!
   return (
     <View className="flex-1 bg-main_bg_color ">
       {/* Hier begint eerst de View bedoeld voor de gehele pagina (body) */}
@@ -99,10 +111,10 @@ export default function ProfileScreen() {
         <Text className="text-wit m-5 text-base flex-2">{accountName}</Text>
       </View>
       <Text className="text-profile-grijs mx-5 text-base -my-7">Account settings</Text>
-      {/* zogenaamde div met account settings komt hieronder met zn eigen view*/}
       <View className="bg-secondary_bg_color my-8 mx-4 h-44 rounded-xl">
+        {/*!--- BEGIN ACOUNT SETTINGS MODAL ---!*/}
         <Pressable onPress={handleAccountPress} style={styles.wrapperCustom}>
-          <Text style={styles.text}>Account</Text>
+          <Text className="text-wit text-base">Account</Text>
         </Pressable>
         <Modal visible={isAccountModalVisible} animationType="slide">
           <View className="flex-1 bg-main_bg_color">
@@ -113,15 +125,15 @@ export default function ProfileScreen() {
                 placeholder="Naam..."
                 placeholderTextColor="grey"
                 value={editedAccountName}
-                onChangeText={(text) => setEditedName(text)}
+                onChangeText={(text) => setEditedAccountName(text)}
                 style={styles.textInput}
               />
-              <Text className="text-wit text-xl">Gebruikersnaam:</Text>
+              <Text className="text-wit text-xl">Foto URL:</Text>
               <TextInput
                 placeholder="Url..."
                 placeholderTextColor="grey"
                 value={editedAccountImageURL}
-                onChangeText={(text) => setEditedImageURL(text)}
+                onChangeText={(text) => setEditedAccountImageURL(text)}
                 style={styles.textInput}
               />
               <View className="my-10">
@@ -136,15 +148,17 @@ export default function ProfileScreen() {
             </View>
           </View>
         </Modal>
+        {/*!--- END ACOUNT SETTINGS MODAL ---!*/}
+        {/*!--- BEGIN CONTACT DETAILS MODAL ---!*/}
         <Pressable
           onPress={handleContactDetailsPress}
           style={[styles.wrapperCustom,]}>
-          <Text style={styles.text}>Contact details</Text>
+          <Text className="text-wit text-base">Contact details</Text>
         </Pressable>
         {/* nu komt de modal voor contact details */}
         <Modal visible={isContactDetailsModalVisible} animationType="slide">
           <View className="flex-1 bg-main_bg_color">
-            <Text className="text-wit text-xl m-5">Edit Details</Text>
+            <Text className="text-wit text-xl m-5">Contact gegevens</Text>
             {/* View zodat je wat margin met de top kan geven */}
             <Pressable onPress={closeContactDetailsModal}>
               <Text className="text-wit text-xl">Save</Text>
@@ -154,19 +168,32 @@ export default function ProfileScreen() {
             </Pressable>
           </View>
         </Modal>
+        {/*!--- END CONTACT DETAILS MODAL ---!*/}
+        {/*!--- BEGIN SECURITY MODAL ---!*/}
         <Pressable
+          onPress={handleSecurityPress}
           style={[styles.wrapperCustom,]}>
-          <Text style={styles.text}>Security</Text>
+          <Text className="text-wit text-base">Security</Text>
         </Pressable>
+        {/* nu komt de modal voor contact details */}
+        <Modal visible={isSecurityModalVisible} animationType="slide">
+          <View className="flex-1 bg-main_bg_color">
+            <Text className="text-wit text-xl m-5">Security</Text>
+            {/* View zodat je wat margin met de top kan geven */}
+            <Pressable onPress={closeSecurityModal}>
+              <Text className="text-wit text-xl">Save</Text>
+            </Pressable>
+            <Pressable onPress={closeSecurityModal}>
+              <Text className="text-wit text-xl my-5">Close</Text>
+            </Pressable>
+          </View>
+        </Modal>
+        {/*!--- END SECURITY MODAL ---!*/}
       </View >
     </View >
   );
 }
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 16,
-    color: 'white',
-  },
   textInput: {
     fontSize: 16,
     color: 'white',
