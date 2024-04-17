fc16b78b2cfa37587326d3d2b9e6c9fabef475a0
MelvinHR <1025457@hr.nl>
2024-04-17 18:32:18 +0200

    Changed Login screen to the one from Figma

--- prototype1/screens/Login.js
+++ prototype1/screens/Login.js
@@ -1,9 +1,10 @@
-import { text } from '@fortawesome/fontawesome-svg-core';
-import { Button, View, Text, TextInput, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import {View, Text, TextInput, TouchableOpacity, Image, ImageBackground } from 'react-native';
 import React, { useState } from 'react';
 import { FontAwesome5 } from '@expo/vector-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import Logo from '../images/SchubergPhilis_White.png'
+import Logo from '../images/SchubergPhilis_White.png';
+import Pant from '../images/Brandpage-Schuberg-Philis.png';
+import { LinearGradient } from 'expo-linear-gradient';
+
 
 
 
@@ -24,54 +25,78 @@ export default function LoginScreen() {
 
   return (
     <View className="flex-1 bg-main_bg_color">
-      <View className="mt-28 items-center mb-5">
-      <Image
-        source={Logo}
-        style={{width: 200, height: 55}}
-      />
-        {/* <View className="bg-third_bg_color rounded-full mr-2 p-5">
-          <FontAwesome5 name="user" size={50} color="white"/>
-        </View> */}
-      </View>
-      <View className="mr-5 ml-5 w-auto h-48 justify-between flex flex-col bg-secondary_bg_color rounded-xl">
-        <TextInput
-          placeholder="Email"
-          value={email}
-          onChangeText={setEmail}
-          placeholderTextColor="grey"
-          className="mt-5 mr-5 ml-5 h-14 w-auto text-base text-wit bg-main_bg_color rounded-md"
-          style={{ paddingLeft: 10, paddingTop: 10, paddingBottom: 10 }}
+      <ImageBackground
+      className="flex-1"
+      source={Pant}
+      imageStyle={{opacity:0.3, width: '200%', }}
+      >
+        <View className="items-center mb-14 mt-44">
+        <Image
+          source={Logo}
+          style={{width: 225, height: 61}}
         />
+        </View>
+        <View className="mr-5 ml-5 w-auto h-48 justify-between flex flex-col bg-secondary_bg_color rounded-xl">
+          <TextInput
+            placeholder="Email"
+            value={email}
+            onChangeText={setEmail}
+            placeholderTextColor="grey"
+            className="mt-5 mr-5 ml-5 h-14 w-auto text-base text-wit bg-main_bg_color rounded-md"
+            style={{ paddingLeft: 10, paddingTop: 10, paddingBottom: 10 }}
+          />
 
-        <View className="mr-5 ml-5 mb-5" >
-          <View className="mb-1 h-14 w-auto bg-main_bg_color rounded-md flex flex-row align-middle relative">
-            <TextInput
-              placeholder="Password"
-              value={password}
-              onChangeText={setPassword}
-              placeholderTextColor="grey"
-              secureTextEntry={!showPassword}
-              className="flex-grow text-wit mr-14"
-              style={{ paddingLeft: 10 }}
-            />
-            <TouchableOpacity onPress={toggleShowPassword} className="absolute right-0 top-0 bottom-0 flex items-center justify-center mr-5">
-              <FontAwesome5
-                name={showPassword ? 'eye-slash' : 'eye'}
-                size={24}
-                color="grey"
+          <View className="mr-5 ml-5 mb-5" >
+            <View className="mb-1 h-14 w-auto bg-main_bg_color rounded-md flex flex-row align-middle relative">
+              <TextInput
+                placeholder="Password"
+                value={password}
+                onChangeText={setPassword}
+                placeholderTextColor="grey"
+                secureTextEntry={!showPassword}
+                className="flex-grow text-wit mr-14"
+                style={{ paddingLeft: 10 }}
               />
+              <TouchableOpacity onPress={toggleShowPassword} className="absolute right-0 top-0 bottom-0 flex items-center justify-center mr-5">
+                <FontAwesome5
+                  name={showPassword ? 'eye-slash' : 'eye'}
+                  size={24}
+                  color="grey"
+                />
+              </TouchableOpacity>
+            </View>
+            <TouchableOpacity onPress={handleLogin} >
+              <Text className="text-[#808080]">Forgot password</Text>
             </TouchableOpacity>
           </View>
-          <TouchableOpacity onPress={handleLogin} >
-            <Text className="text-[#808080]">Forgot password</Text>
+        </View>
+
+
+        <LinearGradient
+            colors={['#1E80ED', '#5FA6F4']}
+            start={{ x: 0, y: 0 }} // Gradient start point (left)
+            end={{ x: 1, y: 0 }}   // Gradient end point (right)
+          className="p-3 m-5 rounded-md"
+        >
+          <TouchableOpacity onPress={handleLogin} className="align-middle">
+            <Text className="text-wit text-center font-bold text-lg">Login</Text>
+          </TouchableOpacity>
+        </LinearGradient>
+
+        <View className="flex-row items-center ml-5 mr-5 mb-5">
+          <View className="flex-1 h-0.5 bg-wit" />
+          <View>
+            <Text className="w-10 text-center text-wit" >Or</Text>
+          </View>
+          <View className="flex-1 h-0.5 bg-wit" />
+        </View>
+        <View className="flex-row justify-center" >
+          <TouchableOpacity >
+            <Text className="text-wit font-bold text-lg underline">Create Account</Text>
           </TouchableOpacity>
         </View>
-      </View>
 
-      <TouchableOpacity onPress={handleLogin} className="bg-schuberg_blue p-3 align-middle mt-10 ml-16 mr-16 rounded-md">
-          <Text className="text-wit text-center font-bold text-lg">Login</Text>
-      </TouchableOpacity>
-
+      </ImageBackground>
     </View>
   );
 }
