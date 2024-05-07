936dd016f764fbb658de9faf964aee7517ef4d5b
maruf128 <maruf@live.nl>
2024-05-07 11:14:03 +0200

    Lees data uit. nog niet zeker of alle laadpalen correct worden uitgelezen

--- go/Api.go
+++ go/Api.go
@@ -1,59 +1,74 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    "net/http"
 	"encoding/json"
-    "bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 func main() {
-    link := "https://schubergphilis.workflows.okta-emea.com/api/flo/d71da429cdb215bef89ffe6448097dee/invoke?clientToken="
-    token := "01d762901510b3c7f422595fa18d8d7bd71c1f3e58ad860fd3ae2d0c87a80955"
-    gegevens := "&url=/poi/v1/locations&method=GET&locationsVisibilityScopes=ACCOUNTS_STATIONS"
-    GetApiData(link, token, gegevens)
+	link := "https://schubergphilis.workflows.okta-emea.com/api/flo/d71da429cdb215bef89ffe6448097dee/invoke?clientToken="
+	token := "01d762901510b3c7f422595fa18d8d7bd71c1f3e58ad860fd3ae2d0c87a80955"
+	gegevens := "&url=/poi/v1/locations&method=GET&locationsVisibilityScopes=ACCOUNTS_STATIONS"
+	GetApiData(link, token, gegevens)
+}
+
+// hier komen de stations dan in
+type StationList struct {
+	StationList []Station `json:"stationList"`
+}
+
+// gegevens van de stations
+type Station struct {
+	ID      string `json:"id"`
+	LocationId string `json:"locationId"`
+	Status  string `json:"status"`
+	Evses   []struct {
+		ID     string `json:"id"`
+		Status string `json:"status"`
+	} `json:"evses"`
 }
 
 func GetApiData(link string, token string, gegevens string) {
-    fullURL := link + token + gegevens
-    req, err := http.NewRequest("GET", fullURL, nil)
-    if err != nil {
-        fmt.Print(err.Error())
-        return
-    }
-    // hoeft niet per se maar is handig voor als je met een API werkt met go
-    req.Header.Add("x-rapidapi-key", token)
-    
-    res, err := http.DefaultClient.Do(req)
-    if err != nil {
-        fmt.Print(err.Error())
-        return
-    }
-    defer res.Body.Close()
-    
-    if res.StatusCode != http.StatusOK {
-        fmt.Printf("Error: API returned status code %d\n", res.StatusCode)
-        return
-    }
-    
-    body, readErr := ioutil.ReadAll(res.Body)
-    if readErr != nil {
-        fmt.Print(readErr.Error())
-        return
-    }
+	fullURL := link + token + gegevens
+	req, err := http.NewRequest("GET", fullURL, nil)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
 
-    // Pretty print JSON 
-    prettyJSON := jsonPrettyPrint(string(body))
-    fmt.Println(prettyJSON)
-}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	defer res.Body.Close()
 
-// zodat data mooit uitleesbaar word in de console (voor nu, word later gemerged met een method die de data omzet in een file of die de data afsplit in methods die dat in een database zet)
-func jsonPrettyPrint(in string) string {
-    var out bytes.Buffer
-    err := json.Indent(&out, []byte(in), "", "\t")
-    if err != nil {
-        return in
-    }
-    return out.String()
+	if res.StatusCode != http.StatusOK {
+		fmt.Printf("Error: API returned status code %d\n", res.StatusCode)
+		return
+	}
+
+	// Decode JSON response
+	var stationList StationList
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return
+	}
+	if err := json.Unmarshal(body, &stationList); err != nil {
+		fmt.Println("Error decoding JSON:", err)
+		return
+	}
+	
+	// Print EVSEs
+	for _, station := range stationList.StationList {
+		fmt.Printf("Station ID: %s, Location ID: %s, Status: %s\n", station.ID, station.LocationId, station.Status)
+		fmt.Println("EVSEs:")
+		for _, evse := range station.Evses {
+			fmt.Printf("  EVSE ID: %s, Status: %s\n", evse.ID, evse.Status)
+		}
+		fmt.Println()
+	}
 }
