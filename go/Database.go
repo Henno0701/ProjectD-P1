8aa593b6c33e0c3dfcbe961b7c9dd02efdc756c1
Henno0701 <henno.passchier@gmail.com>
2024-05-20 15:28:54 +0200

    database.go fix

--- go/Database.go
+++ go/Database.go
@@ -5,7 +5,15 @@ import (
 	"fmt"
 	"strconv"
 	"time"
+	"log"
+	"net/http"
+	"encoding/json"
 )
+
+type Laadpaal struct {
+	ID     int `json:"id"`
+	Status string `json:"status"`
+}
 
 // alle methods/functies die te maken hebben met de database
 func Maketables(db *sql.DB) error {
